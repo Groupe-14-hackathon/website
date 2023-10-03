30409cbd80a96a5b9b9bb04d6bc5af0654c25ed3
ThomasFzr <thfltzr@gmail.com>
2023-10-03 12:51:34 +0200

    debut

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,58 @@
 <template>
-  <h1 class="title">Yfestival</h1>
+   <header>
+        <h1 class="titre" >YFestival 2023</h1>
+        <nav>
+            <ul class="menu">
+                <li><a href="./H">Home</a></li>
+                <li><a href="./Artistes.vue">Artist</a></li>
+                <li><a href="#">Map</a></li>
+                <li><a href="#">Billetterie</a></li>
+                <li><a href="#"><img class ="imgCompte" src="../assets/imgCompte.png"></a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <footer>
+        <p>&copy; 2023 YFestival</p>
+    </footer> 
+
+  
 </template>
 
 <style scoped>
-.title {
-  color: #888;
+.imgCompte{
+    width:25px;
 }
+.titre {
+  color: #e26363;
+}
+
+/* Style pour le menu horizontal */
+.menu {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+    display: flex; /* Utilisation de flexbox pour aligner les éléments horizontalement */
+}
+
+.menu li {
+    margin-right: 20px; /* Espace entre chaque élément du menu (peut être ajusté selon vos préférences) */
+}
+
+.menu li:last-child {
+    margin-right: 0; /* Supprimer la marge à droite du dernier élément */
+}
+
+.menu a {
+    text-decoration: none;
+    color: #333; /* Couleur du texte du lien */
+    font-weight: bold;
+    font-size: 16px;
+}
+
+.menu a:hover {
+    color: #ff6600; /* Couleur du texte du lien au survol */
+}
+
 </style>
+
