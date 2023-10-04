fc5bd8f3fde15b70db379ec7453d846f1c018317
sohkey <thomas.rodriguez38530@gmail.com>
2023-10-04 10:18:29 +0200

    add dashboard

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { reactive } from 'vue'
+
+import Header from './Header.vue';
+import Account from './dashboard/Account.vue'
+import Contact from './dashboard/Contact.vue'
+import Ticket from './dashboard/Ticket.vue'
+import Travel from './dashboard/Travel.vue'
+
+const component = reactive({
+    current: Account,
+})
+
+</script>
+
+<template>
+    <div class="h-screen w-full">
+        <Header></Header>
+        <div class="flex h-full ">
+            <section class="w-[20%] h-full border-r border-red-500 space-y-10">
+                <div class="mt-28 w-full flex justify-center items-center">
+                    <a class="text-xl">Dashboard</a>
+                </div>
+                <div class="pl-5">
+                    <a 
+                    v-on:click="component.current = Account"
+                    class="cursor-pointer border-black hover:border-b">> Account gestion</a>
+                </div>
+                <div class="pl-5">
+                    <a 
+                    v-on:click="component.current = Ticket"
+                    class="cursor-pointer border-black hover:border-b">> Tickets</a>
+                </div>
+                <div class="pl-5">
+                    <a 
+                    v-on:click="component.current = Travel"
+                    class="cursor-pointer border-black hover:border-b">> Book your trip</a>
+                </div>
+                <div class="pl-5">
+                    <a 
+                    v-on:click="component.current = Contact"
+                    class="cursor-pointer border-black hover:border-b">> Contact</a>
+                </div>
+            </section>
+            <section class="w-full h-full">
+                <div class="mt-20">
+                    <component :is="component.current" />
+                </div>
+            </section>
+        </div>
+    </div>
+</template>
---
+++ src/components/dashboard/Account.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="w-full h-full space-y-10">
+        <h1 class="text-center text-xl pt-10">Account gestion</h1>
+        <div class="flex w-full justify-center space-x-10">
+            <div class="w-[20%] flex justify-center border border-red-500 rounded-md">
+                <h1>Change password</h1>
+            </div>
+            <div class="w-[20%] flex justify-center border border-red-500 rounded-md">
+                <h1>Change email</h1>
+            </div>
+            <div class="w-[20%] flex justify-center border border-red-500 rounded-md">
+                <h1>Delete account</h1>
+            </div>
+        </div>
+    </div>
+</template>
