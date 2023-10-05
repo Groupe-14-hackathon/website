096678dd7e5eb4f6452bdd19f0339d2ef065d6f8
manon <manonlebarazer@icloud.com>
2023-10-05 10:16:59 +0200

    front + resp ok

--- src/components/Artistes.vue
+++ src/components/Artistes.vue
@@ -1,14 +1,84 @@
 <script setup>
-import Header from './Header.vue';
+import Header from "./Header.vue";
 </script>
 
 <template>
-    <Header></Header>
-    <div class="title">artists</div>
-</template>
+  <Header></Header>
+  <div id="container-allartist">
+    <div id="container-titleartist">
+      <div class="touring-artist">TOURING ARTIST</div>
+    </div>
+    <div id="container-allcarteartist">
+      <div id="container-carteartistpink">
+        <div id="carteartistpink">
+          <div id="pictureartistpink">
+            <img src="/src/assets/profilpicture.jpg" class="imagepink" />
+          </div>
+          <div class="infoartistpink">INFO ARTIST + TOURNEE EN COURS</div>
+        </div>
+      </div>
+      <div id="container-carteartistaupe">
+        <div id="carteartistaupe">
+          <div id="pictureartistpink">
+            <img src="/src/assets/profilpicture.jpg" class="imagepink" />
+          </div>
+          <div class="infoartistpink">INFO ARTIST + TOURNEE EN COURS</div>
+        </div>
+      </div>
+    </div>
 
-<style>
-.title {
-    color: black;
-}
-</style>
+    <div id="container-titleotherartist">
+      <div class="touring-artist">OTHER ARTIST</div>
+    </div>
+    <div id="container-allcard">
+      <div id="cartes-pink">
+        <div id="pictures-3cartes">
+          <img src="/src/assets/profilpicture.jpg" class="picturecartes" />
+        </div>
+        <div id="container-name">
+          <div class="name">NAME</div>
+        </div>
+      </div>
+      <div id="cartes-taupe">
+        <div id="pictures-3cartes">
+          <img src="/src/assets/profilpicture.jpg" class="picturecartes" />
+        </div>
+        <div id="container-name">
+          <div class="name">NAME</div>
+        </div>
+      </div>
+      <div id="cartes-pink">
+        <div id="pictures-3cartes">
+          <img src="/src/assets/profilpicture.jpg" class="picturecartes" />
+        </div>
+        <div id="container-name">
+          <div class="name">NAME</div>
+        </div>
+      </div>
+      <div id="cartes-taupe">
+        <div id="pictures-3cartes">
+          <img src="/src/assets/profilpicture.jpg" class="picturecartes" />
+        </div>
+        <div id="container-name">
+          <div class="name">NAME</div>
+        </div>
+      </div>
+      <div id="cartes-pink">
+        <div id="pictures-3cartes">
+          <img src="/src/assets/profilpicture.jpg" class="picturecartes" />
+        </div>
+        <div id="container-name">
+          <div class="name">NAME</div>
+        </div>
+      </div>
+      <div id="cartes-taupe">
+        <div id="pictures-3cartes">
+          <img src="/src/assets/profilpicture.jpg" class="picturecartes" />
+        </div>
+        <div id="container-name">
+          <div class="name">NAME</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
