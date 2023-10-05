42cefd62335f5b4a398ec1ddfc2d966007f4229d
sohkey <thomas.rodriguez38530@gmail.com>
2023-10-05 10:21:55 +0200

    forms correction

--- src/components/dashboard/forms/DeleteAccount.vue
+++ src/components/dashboard/forms/DeleteAccount.vue
@@ -8,7 +8,7 @@ const response = reactive({
     message: null,
 })
 
-const delete_account = async ({ password }) => {
+const delete_account = ({ password }) => {
     const token = VueCookies.get('token')
     const id = VueCookies.get('id')
 
@@ -37,24 +37,30 @@ const delete_account = async ({ password }) => {
         <div class="w-[50%] h-[50vh] border border-red-500 rounded-md">
             <div class="h-full flex justify-center items-center flex-col">
                 <h1 class="text-center my-10 text-2xl">Delete account</h1>
-                <div class="flex flex-col space-y-5 w-[50%]">
+                <form 
+                @submit="delete_account({ password })"
+                action=""
+                name="del"
+                target="_blank"
+                method="post"
+                class="flex flex-col space-y-5 w-[50%]">
                     <div class="flex flex-col items-center">
                         <p class="text-center text-red-500">Warning, this operation can't be undone. All your data will be
                             deleted !</p>
                     </div>
                     <div class="flex flex-col items-center">
                         <label class="w-full" for="password">Current Password</label>
-                        <input v-model="password" class="w-full border border-red-500 rounded-md" type="password">
+                        <input required v-model="password" class="w-full border border-red-500 rounded-md" type="password">
                     </div>
                     <div>
-                        <button v-on:click="delete_account({ password })"
+                        <input 
                             class="p-1 border rounded-md border-red-500 hover:bg-red-500 transition-all"
-                            type="submit">Delete</button>
+                            type="submit" value="Delete" />
                     </div>
                     <div>
                         {{ response.message }}
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     </div>
