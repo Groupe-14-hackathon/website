86d38eb0ce09cac8253aaec291c9932f4ba564a7
sohkey <thomas.rodriguez38530@gmail.com>
2023-10-05 17:32:55 +0200

    add frond end for home component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -60,7 +60,7 @@ const closeMenu = () => {
     <nav>
         <div class="navbar">
             <div class="container nav-container">
-                <input class="checkbox" type="checkbox" name="" id="" />
+                <input class="checkbox hidden" type="checkbox" name="" id="" />
                 <div class="hamburger-lines">
                     <span class="line line1"></span>
                     <span class="line line2"></span>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,22 +1,61 @@
 <script setup>
 import Header from './Header.vue';
+import video from '../assets/Hackthon.mp4'
+import cloud from '../assets/clouds-header.png'
+import Jul from '../assets/Jul.jpg'
+import Niska from '../assets/Niska.jpg'
+import PLK from '../assets/PLK.jpg'
+import Orelsan from '../assets/Orelsan.jpg'
+import martingarrix from '../assets/martingarrix.jpg'
+import tomorrow from '../assets/Tomorrowland.jpeg'
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+
+const slides = [
+    { id: '1', img: tomorrow, title: 'TOMORROWLAND', date: '12/10/2022' },
+    { id: '1', img: Jul, title: 'JUL', date: '13/10/2022' },
+    { id: '1', img: Niska, title: 'NISKA', date: '14/10/2022' },
+    { id: '1', img: Orelsan, title: 'ORELSAN', date: '15/10/2022' },
+    { id: '1', img: martingarrix, title: 'MARTIN GARRIX', date: '16/10/2022' },
+    { id: '1', img: PLK, title: 'PLK', date: '17/10/2022' },
+]
 </script>
 
 <template>
-  <Header></Header>
-    <main>
-        COMPILATION <br>
-        DE<br>
-        VIDEOS<br>
-        DE<br>
-        FESTIVAL<br>
-        TROP<br>
-        COOL<br>
-    </main>
+    <Header></Header>
+    <div class="w-full">
+        <div class="video-container">
+            <img class="top-image" :src="cloud">
+            <video class="video" :src="video" loop muted autoplay type="video/mp4"></video>
+            <img class="bottom-image" :src="cloud">
+            <img class="bottom-image " :src="cloud">
+        </div>
+        <div class="h-screen w-full text-black bg-gradient-to-b from-[#FFEAE7] to-purple-300">
+            <h1 class="text-center text-3xl pb-10">News & Dates</h1>
+            <div class="w-full h-fit flex justify-center items-center">
+                <div class="w-[60%] flex space-y-reverse border border-black rounded-md">
+                    <div class="w-full border-r border-black">
+                        <Carousel :wrap-around="true" :autoplay="2000">
+                            <Slide v-for="slide in slides" :key="slide.id">
+                                <div class="carousel__item w-full h-[90vh]">
+                                    <h1 class="absolute font-powerG text-white text-8xl z-10 inset-0">{{ slide.title }}</h1>
+                                    <img
+                                    class="w-full h-full brightness-50"
+                                     :src="slide.img">
+                                     <p class="absolute inset-x-0 bottom-10 text-3xl text-white">{{ slide.date }}</p>
+                                </div>
+                            </Slide>
 
-    <footer>
-        <p>&copy; 2023 YFestival</p>
-    </footer>
+                            <template #addons>
+                                <Navigation/>
+                                <Pagination class="absolute inset-x-0 bottom-0" />
+                            </template>
+                        </Carousel>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 
@@ -30,8 +69,8 @@ import Header from './Header.vue';
 }
 
 main {
-  background-color: #E86252;
-  height: 86.5vh;
+    background-color: #E86252;
+    height: 86.5vh;
 }
 
 .billeterie {
